a75e1ed72ab89de51bcd725c83f6100644fd7749
Joel Justino <justinojjsj@gmail.com>
2024-10-24 00:15:59 -0300

    Update scripts

--- _app/exec.sh
+++ _app/exec.sh
@@ -8,7 +8,7 @@ data_hora=$(date '+%d/%m/%y %H:%M:%S')
 espaco=" "
 
 #SELECIONA O ARQUIVO QUE IRÁ SALVAR
-nome_do_pc=$(/usr/bin/hostname)
+nome_do_pc=$(cat nome_host.txt)
 
 if [ "$nome_do_pc" = "montech" ]; then
     nome_arquivo_log="hora_executada_montech.log"
@@ -20,7 +20,7 @@ fi
 
 #SCRIPT
 cd /app
-$python_e $app_ccr #executa app_ccr.py
+#$python_e $app_ccr #executa app_ccr.py
 echo $app_ccr$espaco$data_hora >> $nome_arquivo_log #salva horario de execucao
-$python_e $app_classificador
+#$python_e $app_classificador
 echo $app_classificador$espaco$data_hora >> $nome_arquivo_log
