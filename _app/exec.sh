0ee2953134016ffc67b25d1769e9c5f8cb22321f
Joel Justino <justinojjsj@gmail.com>
2024-10-23 23:09:27 -0300

    Update Shell Scripts to identified Hostnames

--- _app/exec.sh
+++ _app/exec.sh
@@ -7,9 +7,20 @@ app_classificador="/app/classificador.py"
 data_hora=$(date '+%d/%m/%y %H:%M:%S')
 espaco=" "
 
+#SELECIONA O ARQUIVO QUE IRÁ SALVAR
+nome_do_pc=$(/usr/bin/hostname)
+
+if [ "$nome_do_pc" = "montech" ]; then
+    nome_arquivo_log="hora_executada_montech.log"
+elif [ "$nome_do_pc" = "capturador" ]; then
+    nome_arquivo_log="hora_executada_capturador.log"
+else
+    nome_arquivo_log="hora_executada_outropc.log"
+fi
+
 #SCRIPT
 cd /app
 $python_e $app_ccr #executa app_ccr.py
-echo $app_ccr$espaco$data_hora >> hora_executada.log #salva horario de execucao
+echo $app_ccr$espaco$data_hora >> $nome_arquivo_log #salva horario de execucao
 $python_e $app_classificador
-echo $app_classificador$espaco$data_hora >> hora_executada.log
+echo $app_classificador$espaco$data_hora >> $nome_arquivo_log
--- _app/exec2.sh
+++ _app/exec2.sh
@@ -6,7 +6,18 @@ app_classificador_temporal="/app/classificador_temporal.py"
 data_hora=$(date '+%d/%m/%y %H:%M:%S')
 espaco=" "
 
+#SELECIONA O ARQUIVO QUE IRÁ SALVAR
+nome_do_pc=$(/usr/bin/hostname)
+
+if [ "$nome_do_pc" = "montech" ]; then
+    nome_arquivo_log="hora_executada_montech.log"
+elif [ "$nome_do_pc" = "capturador" ]; then
+    nome_arquivo_log="hora_executada_capturador.log"
+else
+    nome_arquivo_log="hora_executada_outropc.log"
+fi
+
 #SCRIPT
 cd /app
 $python_e $app_classificador_temporal
-echo $app_classificador_temporal$espaco$data_hora >> hora_executada.log #salva horario de execucao
+echo $app_classificador_temporal$espaco$data_hora >> $nome_arquivo_log #salva horario de execucao
